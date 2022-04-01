fa66c8077d0c640798cf2cfbbae311edeb565b3a
jonlam1989 <jonlam1989@outlook.com>
2022-04-01 10:39:27 -0400

    added msg for Add_To_Cart button in Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 //Styles
 import styles from '../css/Item.module.css';
 //Custom hook for context
@@ -6,11 +6,27 @@ import { useGlobalContext } from '../context/context';
 
 function Item(props) {
 	const { id, name, price, src, alt } = props;
-	const { addToCart } = useGlobalContext();
+	const { cart, addToCart } = useGlobalContext();
+	const msg = useRef('');
 	const imgStyles = name.includes('KT Tape') ? styles.itemImgContain : styles.itemImgCover;
 
+	const handleClick = (id) => {
+		const isProductInCart = cart.find((item) => item.id === id);
+		if (!isProductInCart) {
+			addToCart(id);
+			msg.current.innerHTML = 'Successfully added to cart!';
+		} else {
+			msg.current.innerHTML = 'Item is already in cart';
+		}
+		setTimeout(() => (msg.current.innerHTML = ''), 3000);
+	};
+
 	return (
-		<div>
+		<div className={styles.itemContainer}>
+			<div className={styles.itemMsg}>
+				<p ref={msg} />
+			</div>
+
 			<img src={src} alt={alt} className={imgStyles} />
 
 			<div className={styles.itemDetails}>
@@ -19,7 +35,7 @@ function Item(props) {
 					<h2>${price}</h2>
 				</div>
 				<div>
-					<button onClick={() => addToCart(id)}>Add to Cart</button>
+					<button onClick={() => handleClick(id)}>Add to Cart</button>
 				</div>
 			</div>
 		</div>
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -6,14 +6,8 @@ const reducer = (state, action) => {
 		case 'SET_CATEGORY':
 			return { ...state, category: action.payload };
 		case 'ADD_TO_CART':
-			const isProductInCart = state.cart.find((item) => item.id === action.payload);
-
-			if (isProductInCart) {
-				return state;
-			} else {
-				const item = allProducts.find((item) => item.id === action.payload);
-				return { ...state, cart: [ ...state.cart, item ], cartAmount: state.cartAmount + 1 };
-			}
+			const item = allProducts.find((item) => item.id === action.payload);
+			return { ...state, cart: [ ...state.cart, item ], cartAmount: state.cartAmount + 1 };
 		default:
 			throw new Error();
 	}
