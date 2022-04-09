d15cc121116e25ea3ccd0e139829587ba239e81f
jonlam1989 <jonlam1989@outlook.com>
2022-04-08 21:08:03 -0400

    replaced msg with Notification for Add_To_Cart button in Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 //Styles
 import styles from '../css/Item.module.css';
+//Components
+import Notification from './Notification';
 //Custom hook for context
 import { useGlobalContext } from '../context/context';
 
@@ -8,8 +10,6 @@ function Item(props) {
 	const { id, name, price, src, alt, largeImage } = props;
 	const { cart, addToCart, toggleModal } = useGlobalContext();
 	const imgStyles = largeImage ? styles.itemImgContain : styles.itemImgCover;
-	//for creating an alert message when adding item to cart
-	const msg = useRef('');
 	//for creating a setTimeout to remove the alert message
 	const timerRef = useRef(null);
 
@@ -17,11 +17,11 @@ function Item(props) {
 		const isProductInCart = cart.find((item) => item.id === id);
 		if (!isProductInCart) {
 			addToCart(id);
-			msg.current.innerHTML = 'Successfully added to cart!';
+			// msg.current.innerHTML = 'Successfully added to cart!';
 		} else {
-			msg.current.innerHTML = 'Item is already in cart';
+			// msg.current.innerHTML = 'Item is already in cart';
 		}
-		timerRef.current = setTimeout(() => (msg.current.innerHTML = ''), 2000);
+		// timerRef.current = setTimeout(() => (msg.current.innerHTML = ''), 2000);
 	};
 
 	//https://felixgerschau.com/react-hooks-settimeout/
@@ -33,7 +33,7 @@ function Item(props) {
 	return (
 		<div className={styles.itemContainer}>
 			<div className={styles.itemMsg}>
-				<p ref={msg} />
+				<Notification type='success' message='Successfully added to cart!' />
 			</div>
 
 			<img src={src} alt={alt} className={imgStyles} onClick={() => toggleModal({ ...props })} />
