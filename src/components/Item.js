bfb9f7086c5083675d81c3c9e3d89c157268e623
jonlam1989 <jonlam1989@outlook.com>
2022-04-08 21:34:11 -0400

    added local state (notification, showNotification) to Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 //Styles
 import styles from '../css/Item.module.css';
 //Components
@@ -10,6 +10,9 @@ function Item(props) {
 	const { id, name, price, src, alt, largeImage } = props;
 	const { cart, addToCart, toggleModal } = useGlobalContext();
 	const imgStyles = largeImage ? styles.itemImgContain : styles.itemImgCover;
+	//for creating and displaying alert message
+	const [ notification, setNotification ] = useState({ type: '', message: '' });
+	const [ showNotification, setShowNotification ] = useState(false);
 	//for creating a setTimeout to remove the alert message
 	const timerRef = useRef(null);
 
@@ -17,11 +20,12 @@ function Item(props) {
 		const isProductInCart = cart.find((item) => item.id === id);
 		if (!isProductInCart) {
 			addToCart(id);
-			// msg.current.innerHTML = 'Successfully added to cart!';
+			setNotification({ type: 'success', message: 'Successfully added to cart!' });
 		} else {
-			// msg.current.innerHTML = 'Item is already in cart';
+			setNotification({ type: 'info', message: `${name} is already in cart` });
 		}
-		// timerRef.current = setTimeout(() => (msg.current.innerHTML = ''), 2000);
+		setShowNotification(true);
+		timerRef.current = setTimeout(() => setShowNotification(false), 2000);
 	};
 
 	//https://felixgerschau.com/react-hooks-settimeout/
@@ -32,9 +36,7 @@ function Item(props) {
 
 	return (
 		<div className={styles.itemContainer}>
-			<div className={styles.itemMsg}>
-				<Notification type='success' message='Successfully added to cart!' />
-			</div>
+			<div className={styles.itemMsg}>{showNotification && <Notification {...notification} />}</div>
 
 			<img src={src} alt={alt} className={imgStyles} onClick={() => toggleModal({ ...props })} />
 
