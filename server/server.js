c801d00d05c491c93638baa427fb848c37039e4c
jonlam1989 <jonlam1989@outlook.com>
2022-06-18 13:03:03 -0400

    added nodemon

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+//routes
+app.get('/api/', (req, res) => {
+	res.json({ msg: 'hello world!' });
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`server started on port: ${PORT}`);
 });
