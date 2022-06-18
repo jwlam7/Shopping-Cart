4e67bfdb76f7060a85fd8ec9a3a927618c25364a
jonlam1989 <jonlam1989@outlook.com>
2022-06-18 17:49:07 -0400

    added dotenv

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 //routes
 app.get('/api/', (req, res) => {
-	res.json({ msg: 'hello world!' });
+	res.json({ msg: 'hello world' });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`server started on port: ${PORT}`);
 });
