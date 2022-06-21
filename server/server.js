e8298b179a986c45d47152e5808716472e243d33
jonlam1989 <jonlam1989@outlook.com>
2022-06-20 20:14:49 -0400

    connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+//DB connection
+const connectDB = require('./db/connect');
 
 //routes
 app.get('/api/', (req, res) => {
@@ -8,6 +10,15 @@ app.get('/api/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-	console.log(`server started on port: ${PORT}`);
-});
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGODB_URI);
+		app.listen(PORT, () => {
+			console.log(`server started on port: ${PORT}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
