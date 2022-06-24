9d9d11e574acfd66247ba540ab14386833b291a0
jonlam1989 <jonlam1989@outlook.com>
2022-06-24 17:31:19 -0400

    updated products controller to sort by category

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/Product');
 
 const getAllProducts = async (req, res) => {
 	try {
-		const products = await Product.find({});
+		const products = await Product.find({}).sort('category');
 		res.status(200).json({ products });
 	} catch (error) {
 		console.log(error);
